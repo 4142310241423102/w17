1f35ed78e36d29ccb5cb2bfe889c8b1455c2393a
4142310241423102 <41423102@nfu.edu.tw>
2025-12-30 20:13:45 +0800

    w3_練習2

---
+++ w3練習_2.py
@@ -0,0 +1,8 @@
+﻿
+total = 0
+
+for i in range(2, 101, 2):          # 取 2~100 的偶數
+    if '3' not in str(i):            # 如果數字 i 裡面沒有「3」
+        total += i                   # 才把 i 加進總和
+
+print(total)
